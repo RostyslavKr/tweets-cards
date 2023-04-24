9cd949863f2d3a8aef24e90e1c4974edf52813e3
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-04-24 14:30:22 +0000

    Deploying to gh-pages from @ RostyslavKr/tweets-cards@aa71fa5400fa2873a689f805293e6108bb29df91 🚀

---
+++ static/js/415.b3959448.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweets_cards=self.webpackChunktweets_cards||[]).push([[415],{415:function(e,s,t){t.r(s);var c=t(184);s.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Hello, welcome to Home Page"})})}}}]);
+//# sourceMappingURL=415.b3959448.chunk.js.map
